6dcb268c3ead175bedb7df3e0d618462021bcd3d
dakshpokar <dakshpokar@gmail.com>
2019-03-04 16:33:03 +0530

    Post Method for Registration

--- TaskManager/TM/views.py
+++ TaskManager/TM/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .forms import LoginForm, UserForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
+
 class MainView(TemplateView):
     template_name = "index.html"
     def get(self, request):
@@ -22,4 +26,17 @@ class RegisterView(TemplateView):
         form = UserForm()
         return render(request, self.template_name, {'form': form})
     def post(self, request):
-        return
+        if request.method == "POST":
+            form = UserForm(request.POST, request.FILES)
+            if form.is_valid():
+                first_name = form.cleaned_data["first_name"]
+                last_name = form.cleaned_data["last_name"]
+                email = form.cleaned_data["email"]
+                password = form.cleaned_data["password"]
+                user = User.objects.create_user(username=email,password=password)
+                user.first_name = first_name
+                user.last_name = last_name
+                user.save()
+                return redirect('../login')
+            else:
+                return redirect('../')
