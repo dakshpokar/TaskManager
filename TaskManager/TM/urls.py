dfecdf51c345bd6602ec5c3c3bc35f47cf9d16b3
dakshpokar <dakshpokar@gmail.com>
2019-03-06 23:13:53 +0530

    Fixed few bugs

--- TaskManager/TM/urls.py
+++ TaskManager/TM/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
-from TM.views import LoginView, RegisterView, DashboardView, CreateTeamView, logout_view, TeamView, ProfileView, SpecificTeamView, CreateTaskView
+from TM.views import LoginView, RegisterView, DashboardView, CreateTeamView, logout_view, TeamView, ProfileView, SpecificTeamView, CreateTaskView, TasksView, DeleteTeam, MembersView, TeamSettingsView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -12,6 +12,8 @@ urlpatterns = [
     url(r'^profile\/.*$', ProfileView.as_view(), name="profile"),
     url(r'^team\/[a-zA-z0-9]+\/dashboard\/$', SpecificTeamView.as_view(), name="team"),    
     url(r'^team\/[a-zA-z0-9]+\/create-task\/$', CreateTaskView.as_view(), name="create-tasks"),    
-   
-    
+    url(r'^team\/[a-zA-z0-9]+\/tasks\/$', TasksView.as_view(), name="tasks"),
+    url(r'^team\/[a-zA-z0-9]+\/members\/$', MembersView.as_view(), name="members"),
+    url(r'^team\/[a-zA-z0-9]+\/settings\/$', TeamSettingsView.as_view(), name="settings"),
+    url(r'^team\/[a-zA-z0-9]+\/delete\/$', DeleteTeam.as_view(), name="delete"),
 ]
--- TaskManager/TM/views.py
+++ TaskManager/TM/views.py
@@ -109,7 +109,11 @@ class TeamView(LoginRequiredMixin, TemplateView):
         user = request.user
         us = UserProfile.objects.get(user=user)
         teams = Teams.objects.filter(admin=user)
-        return render(request, self.template_name, {'teams': teams, 'us': us, 'user': user})
+        joined_teams = Membership.objects.filter(member=User.objects.get(username=user.username))
+        jt = None
+        if joined_teams.all().count() == 1:
+            jt = joined_teams.all()[0].team
+        return render(request, self.template_name, {'teams': teams, 'us': us, 'user': user, 'joined_teams': joined_teams, 'jt': jt})
     def post(self, request):
         return
 
@@ -188,3 +192,28 @@ class TeamSettingsView(LoginRequiredMixin, TemplateView):
         return render(request, self.template_name, {'user': user, 'us': us, 'team': team})
     def post(self, request):
         return
+
+class DeleteTeam(LoginRequiredMixin, TemplateView):
+    template_name="team/delete.html"
+    login_url='/../login'
+    def get(self, request):
+        user = request.user
+        us = UserProfile.objects.get(user=user)
+        team = Teams.objects.get(url=request.path.split("/")[2])
+        form = LoginForm()
+        return render(request, self.template_name, {'user': user, 'us': us, 'form': form, 'team': team})
+    def post(self, request):
+        user = request.user
+        us = UserProfile.objects.get(user=user)
+        team = Teams.objects.get(url=request.path.split("/")[2])
+        form = LoginForm(request.POST)
+        if request.method == "POST":
+            if form.is_valid():
+                email_addr = form.cleaned_data["email"]
+                password = form.cleaned_data["password"]
+                user = authenticate(request, username=email_addr, password=password)
+                if user is not None:
+                    team.delete()
+                    return redirect("/")
+                else:
+                    return render(request, self.template_name, {'user': user, 'us': us, 'form': form, 'team': team, 'error': True, 'error_msg': "Wrong Credentials"})
