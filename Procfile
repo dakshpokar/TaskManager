9fccd7ca0b1b6cac8edb4938b5e7daa9cfac9b35
dakshpokar <dakshpokar@gmail.com>
2019-03-18 21:21:22 +0530

    Final

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python TaskManager/manage.py runserver 0.0.0.0:$PORT
+web: gunicorn TaskManager.wsgi
--- TaskManager/TaskManager/settings.py
+++ TaskManager/TaskManager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -46,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -129,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
