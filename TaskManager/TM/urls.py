301988da0394bb5a4ec317b22a4adf1f58dcf69d
Daksh Pokar <dakshp2@illinois.edu>
2024-05-06 19:17:42 -0500

    chore: Add .gitignore file and update footer copyright year and author name

--- TaskManager/TM/urls.py
+++ TaskManager/TM/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from TM.views import *
 
 urlpatterns = [
@@ -12,18 +11,18 @@ urlpatterns = [
     path('settings/', SettingsView.as_view(), name="settings"),
     path('about/', AboutView.as_view(), name="about"),
 
-    url(r'^profile\/.*$', ProfileView.as_view(), name="profile"),
-    url(r'^team\/[a-zA-Z0-9]+\/dashboard\/$', SpecificTeamView.as_view(), name="team"),    
-    url(r'^team\/[a-zA-Z0-9]+\/create-task\/$', CreateTaskView.as_view(), name="create-tasks"),    
-    url(r'^team\/[a-zA-Z0-9]+\/tasks\/$', TasksView.as_view(), name="tasks"),
-    url(r'^team\/[a-zA-Z0-9]+\/members\/$', MembersView.as_view(), name="members"),
-    url(r'^team\/[a-zA-Z0-9]+\/settings\/$', TeamSettingsView.as_view(), name="settings"),
-    url(r'^team\/[a-zA-Z0-9]+\/delete\/$', DeleteTeam.as_view(), name="delete"),
-    url(r'^team\/[a-zA-Z0-9]+\/tasks\/[a-zA-Z0-9]+\/delete\/$', DeleteTasks.as_view(), name="delete"),
-    url(r'^team\/[a-zA-Z0-9]+\/tasks\/[a-zA-Z0-9]+\/$', SpecificTaskView.as_view(), name="specific-task"),
-    url(r'^team\/[a-zA-Z0-9]+\/member\/[a-zA-Z0-9]+\/delete\/$', DeleteTeamMember.as_view(), name="members"),
-    url(r'^team\/[a-zA-Z0-9]+\/member\/[a-zA-Z0-9]+\/deleteself\/$', DeleteTeamMember.as_view(), name="members"),
-    url(r'^team\/[a-zA-Z0-9]+\/tasks\/[a-zA-Z0-9]+\/settings/$', TaskSettings.as_view(), name="specific-task"),
+    re_path(r'^profile\/.*$', ProfileView.as_view(), name="profile"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/dashboard\/$', SpecificTeamView.as_view(), name="team"),    
+    re_path(r'^team\/[a-zA-Z0-9]+\/create-task\/$', CreateTaskView.as_view(), name="create-tasks"),    
+    re_path(r'^team\/[a-zA-Z0-9]+\/tasks\/$', TasksView.as_view(), name="tasks"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/members\/$', MembersView.as_view(), name="members"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/settings\/$', TeamSettingsView.as_view(), name="settings"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/delete\/$', DeleteTeam.as_view(), name="delete"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/tasks\/[a-zA-Z0-9]+\/delete\/$', DeleteTasks.as_view(), name="delete"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/tasks\/[a-zA-Z0-9]+\/$', SpecificTaskView.as_view(), name="specific-task"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/member\/[a-zA-Z0-9]+\/delete\/$', DeleteTeamMember.as_view(), name="members"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/member\/[a-zA-Z0-9]+\/deleteself\/$', DeleteTeamMember.as_view(), name="members"),
+    re_path(r'^team\/[a-zA-Z0-9]+\/tasks\/[a-zA-Z0-9]+\/settings/$', TaskSettings.as_view(), name="specific-task"),
 
 
 ]
